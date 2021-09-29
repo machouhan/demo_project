bfc50946e1b84af933f60355addec6f89670fac3
Mayuri Chouhan <machouhan@bestpeers.com>
2021-09-29 17:22:13 +0530

    login page design and user model modify and added fields in signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do  
+     get '/users/sign_out' => 'devise/sessions#destroy'     
+  end 
+  root to: "products#index"
 
-  root to: "home#index"
 end
